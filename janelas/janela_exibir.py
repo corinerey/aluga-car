7f3ef5ab1b78eeb3cfa9d0ca9fd8898462426137
Isabelle <corinedelrey@gmail.com>
2024-05-17 11:11:19 -0300

    feat: criaçao das janelas

--- janelas/janela_exibir.py
+++ janelas/janela_exibir.py
@@ -4,8 +4,17 @@ class ExibirVeiculos(ctk.CTkToplevel):
     def __init__(self, master=None):
         super().__init__(master)
         self.title('Visualização de Veículos')
-        self.geometry('600x800')
+        self.geometry('800x600')
         self.resizable(False, False)
 
-        self.label_visualizar = ctk.CTkLabel(text="Janela Visualizar")
-        self.label_visualizar.pack()
+        self.frameTitulo = ctk.CTkFrame(self, width=800, height=100, corner_radius=0)
+        self.frameTitulo.pack(fill='both', expand=False)
+        self.frameTitulo.pack_propagate(False)
+
+        self.frameTabela = ctk.CTkFrame(self, width=800, height=500, corner_radius=0)
+        self.frameTabela.pack(fill='both', expand=False)
+        self.frameTabela.pack_propagate(False)
+
+        self.labelTitulo = ctk.CTkLabel(self.frameTitulo, text='Visualizar Veiculos', 
+        font=('Open Sans', 26, 'bold'), text_color='white')
+        self.labelTitulo.pack(side='bottom')
